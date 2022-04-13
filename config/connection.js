e739d8db5c0bf19fb98feecce833c5e3972da8eb
Roberto Monplaisir <castedcrown84@gmail.com>
2022-04-12 23:43:56 -0400

    set up sequelize connection and added to server file

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+
+let sequelize;
+
+if(JAWSDB_URL){
+
+sequelize = new Sequelize(JAWSDB_URL)
+
+}else{
+
+sequelize = new Sequelize(
+
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3030
+
+    }
+
+)}
+
+module.exports = sequelize
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./config/connection')
 
 const Port = process.env.Port || 3001;
 const app = express();
@@ -7,3 +8,17 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+
+
+
+sequelize.sync({force:true}).then(() => {
+
+app.listen(Port, ()=> {
+
+`Listening to server!`
+
+})
+
+
+})
