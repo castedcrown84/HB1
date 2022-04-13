6d00609d2708180cf8f10008bea7b54c1c914ba5
Roberto Monplaisir <castedcrown84@gmail.com>
2022-04-13 00:28:08 -0400

    finishing setting up server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
 const sequelize = require('./config/connection')
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const Port = process.env.Port || 3001;
 const app = express();
 
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars' )
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+app.use(require('./controllers/dish-routes'));
+
+
 
 
 
