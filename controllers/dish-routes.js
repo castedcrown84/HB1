3226aead58bd6898913f1c92e90b78d0b3f84a65
Roberto Monplaisir <castedcrown84@gmail.com>
2022-04-13 22:33:47 -0400

    added more content to controllers

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
 try{
 
 const dishdata = await Dish.findAll();
-
+//Serialize the Data and converted it to a plain object
 const dishes = dishdata.map((dish) => { dish.get({plain:true})}  )
 
 res.render('all', dishes)
@@ -18,4 +18,30 @@ res.render('all', dishes)
 res.status(404).json(err);
 
 }
+})
+
+router.get('/dish/:id', async (req, res) => {
+
+try{
+
+const dishdata = await Dish.findByPk(req.params.id);
+
+if(!dishdata){
+
+res.status(404).json({message: "id not found"})
+}
+
+
+//serialize data to render it
+const dish = dishdata.get({plain:true})
+
+res.render('dish', {dish})
+
+
+}catch(err){
+
+    res.status(400).json(err)
+}
+
+
 })
