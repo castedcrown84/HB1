3915eefb23c03b3234c209e037fe5a98077e3b45
Roberto Monplaisir <castedcrown84@gmail.com>
2022-04-15 12:37:05 -0400

    made some corrections on the server

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -1,50 +1,42 @@
 const router = require('express').Router();
-const Dish = require('../models/Dish');
+const Dish = require('../models/Dish')
 
 
 
 router.get('/', async (req, res) => {
 
-try{
+const dishdata = await Dish.findAll().catch((err) => {
 
-const dishdata = await Dish.findAll();
-//Serialize the Data and converted it to a plain object
-const dishes = dishdata.map((dish) => { dish.get({plain:true})}  )
+    res.json(err)
+})
 
-res.render('all', {dishes})
+const dishes = dishdata.map((dish) => dish.get({plain:true}))
 
-}catch(err){
+res.render('all', { dishes })
 
-res.status(404).json(err);
-
-}
 })
 
 router.get('/dish/:id', async (req, res) => {
 
 try{
 
-const dishdata = await Dish.findByPk(req.params.id);
+const dishdata = await Dish.findByPk(req.params.id)
 
 if(!dishdata){
 
-res.status(404).json({message: "id not found"})
+    res.json({message: "ID not found"})
+
 }
 
-
-//serialize data to render it
 const dish = dishdata.get({plain:true})
 
-res.render('dish', {dish})
+res.render('dish', dish)
 
 
 }catch(err){
 
-    res.status(400).json(err)
+     res.json(err)
 }
-
-
 })
 
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-const express = require('express')
-const path = require('path')
-const sequelize = require('./config/connection')
+const path = require('path');
+const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
-const Port = process.env.Port || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars' )
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/dish-routes'));
 
-
-
-
-sequelize.sync({force:false}).then(() => {
-
-app.listen(Port, ()=> {
-
-console.log(`Listening to server!`)
-
-})
-
-
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
